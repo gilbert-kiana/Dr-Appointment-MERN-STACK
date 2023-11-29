98e387b0629969f192ac52450b85bcf60088030f
Alan Peter <petera3@gmatc.matc.edu>
2023-11-29 19:01:58 +0300

    our great doctors section done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,6 +12,7 @@ import { Link } from 'react-router-dom'
 import { BsArrowRight } from "react-icons/bs";
 import About from '../components/About/About'
 import ServiceList from '../components/services/ServiceList'
+import DoctorList from '../components/Doctors/DoctorList'
 
 const Home = () => {
     return (
@@ -210,6 +211,19 @@ const Home = () => {
                                 </div>
                             </div>
                         </div>
+                    </div>
+                </section>
+
+                {/* Doctors */}
+                <section>
+                    <div className="container">
+                        <div className="xl:w-[470px] mx-auto">
+                            <h2 className="heading text-center ">Our great Doctors</h2>
+                            <p className="text__para text-center">
+                                World-class care for everyone. Our health system offers unmatched,expert health Care.
+                            </p>
+                        </div>
+                        <DoctorList />
                     </div>
                 </section>
             </>
