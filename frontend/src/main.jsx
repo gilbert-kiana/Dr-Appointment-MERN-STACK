9e1e754445976e8e5025babfa8ad0792d0c615a5
Gilbert Intabo <gilbertintabo@gmail.com>
2024-01-25 12:30:25 +0300

    added context provider wrapper

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,12 +5,15 @@ import 'react-toastify/dist/ReactToastify.css';
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { AuthContextProvider } from './context/AuthContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ToastContainer theme="dark" position="top-right" autoClose={3000} closeOnClick pauseOnHover={false} />
-      <App />
+      <AuthContextProvider>
+        <ToastContainer theme="dark" position="top-right" autoClose={3000} closeOnClick pauseOnHover={false} />
+        <App />
+      </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
