02e238d376fcf23990b34d090f092db85bba3568
Gilbert Intabo <gilbertintabo@gmail.com>
2024-01-25 12:34:21 +0300

    the new endpoints to the router

--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -4,6 +4,8 @@ import {
   singleUser,
   deleteUser,
   updateUser,
+  getUserProfile,
+  getMyAppointments,
 } from "../Controllers/userController.js";
 import { authenticate, restrict } from "../auth/verifyToken.js";
 
@@ -13,5 +15,12 @@ router.get("/", authenticate, restrict(["admin"]), getAllUsers);
 router.get("/:id", authenticate, restrict(["patient"]), singleUser);
 router.put("/:id", authenticate, restrict(["patient"]), updateUser);
 router.delete("/:id", authenticate, restrict(["patient"]), deleteUser);
+router.get("/profile/me", authenticate, restrict(["patient"]), getUserProfile);
+router.get(
+  "/appointments/my-appointments",
+  authenticate,
+  restrict(["patient"]),
+  getMyAppointments
+);
 
 export default router;
