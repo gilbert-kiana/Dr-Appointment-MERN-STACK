1c63b714dd21f432f06bc9255fea1b91191a434e
Gilbert Intabo <gilbertintabo@gmail.com>
2024-01-25 12:32:48 +0300

    added getdoctorprofile endpoint

--- backend/Controllers/doctorController.js
+++ backend/Controllers/doctorController.js
@@ -1,3 +1,4 @@
+import Booking from "../models/BookingSchema.js";
 import Doctor from "../models/DoctorSchema.js";
 
 export const updateDoctor = async (req, res) => {
@@ -77,5 +78,29 @@ export const getAllDoctor = async (req, res) => {
     });
   } catch (error) {
     res.status(404).json({ success: false, message: error });
+  }
+};
+
+export const getDoctorProfile = async (req, res) => {
+  const doctorId = req.userId;
+
+  try {
+    const doctor = await Doctor.findById(doctorId);
+
+    if (!doctor) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Doctor not Found" });
+    }
+    const { password, ...rest } = doctor._doc;
+    const appointments = await Booking.find({ doctor: doctorId });
+
+    res.status(200).json({
+      success: true,
+      message: "Profile info retrieved",
+      data: { ...rest, appointments },
+    });
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Something went wrong." });
   }
 };
