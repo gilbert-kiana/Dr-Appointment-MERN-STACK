8e202301d88597dbc79a262564a8ca2f2eac653a
gilbert-kiana <gilbertintabo@gmail.com>
2024-01-29 11:07:58 +0300

    Logout functionality

--- frontend/src/dashboard/doctor-account/Tabs.jsx
+++ frontend/src/dashboard/doctor-account/Tabs.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { BiMenu } from "react-icons/bi"
+import { authContext } from '../../context/AuthContext'
+import { useNavigate } from 'react-router-dom'
 
 const Tabs = ({ tab, setTab }) => {
+
+    const { dispatch } = useContext(authContext)
+    const navigate = useNavigate()
+
+    const handleLogout = () => {
+        dispatch({ type: 'LOGOUT' })
+        navigate('/')
+    }
     return (
         <div>
             <span className="lg:hidden">
@@ -23,12 +33,21 @@ const Tabs = ({ tab, setTab }) => {
                         } w-full btn mt-0 rounded-md`}
                 >Appointments</button>
                 <button
-                    onClick={() => setTab('settings')}
-                    className={`${tab === 'settings'
+                    onClick={() => setTab('profile')}
+                    className={`${tab === 'profile'
                         ? 'bg-indigo-100 text-primaryColor'
                         : 'bg-transparent text-headingColor'
                         } w-full btn mt-0 rounded-md`}
-                >Settings</button>
+                >Profile</button>
+
+                <div className="mt-[100px] w-full">
+                    <button onClick={handleLogout} className="w-full bg-[#181A1E] p-3 text-[16px] leading-7 rounded-md text-white">
+                        Logout
+                    </button>
+                    <button className="w-full bg-red-600 mt-4 p-3 text-[16px] leading-7 rounded-md text-white">
+                        Delete Account
+                    </button>
+                </div>
             </div>
         </div>
     )
